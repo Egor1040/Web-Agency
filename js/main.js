2bc74b590b9c2e6d01ce2e9672d3ad129e5c32e9
Igor <igor.motriy3@gmail.com>
2023-01-23 22:00:05 +0200

    fix menu add js

--- js/main.js
+++ js/main.js
@@ -8,3 +8,24 @@ window.onscroll = () => {
         menu.classList.remove('menu_fixed');
     }
 };
+
+document.addEventListener("click", documentAction);
+
+function documentAction(e) {
+    const targetElement = e.target;
+
+    if (targetElement.closest('[data-goto]')) {
+
+        const goTo = targetElement.closest('[data-goto]').dataset.goto;
+        const goToElement = document.querySelector(goTo);
+        const headerHeight = document.querySelector('.header').offsetHeight;
+
+        if (goToElement) {
+            window.scrollTo({
+                top: goToElement.offsetTop - (headerHeight + 15),
+                behavior: "smooth"
+            })
+        }
+        e.preventDefault();
+    }
+}
